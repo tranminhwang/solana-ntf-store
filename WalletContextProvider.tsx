af37968f89beb14d37c9dfb739b3ff453f049666
tranminhwang <j2team.tranminhquang@gmail.com>
2023-02-25 16:23:56 +0700

    final

---
+++ WalletContextProvider.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const url = useMemo(() => clusterApiUrl("devnet"), []);
+  const wallets = [new PhantomWalletAdapter()];
+
+  return (
+    <ConnectionProvider endpoint={url}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
